8879ea721c5fab7a8da8f555db4642cb1e1f731b
Yuanze Lin <yuanze.lin@cs.ox.ac.uk>
2026-06-04 16:42:40 +0100

    Update download_illumicraft_weights.py

--- download_illumicraft_weights.py
+++ download_illumicraft_weights.py
@@ -12,6 +12,7 @@ snapshot_download(
     repo_id="YuanzeLin/Illumicraft-checkpoints",
     local_dir="checkpoints/illumicraft_weights",
     local_dir_use_symlinks=False,
+    resume_download=True,
 )
 
 print("Downloaded to checkpoints/illumicraft_weights")
