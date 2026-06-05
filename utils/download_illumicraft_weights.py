2e55609ad4cc2adae452508b40bcdd587c39f886
Yuanze Lin <yuanze.lin@cs.ox.ac.uk>
2026-06-06 00:12:15 +0100

    download_illumicraft_weights.py

---
+++ utils/download_illumicraft_weights.py
@@ -0,0 +1,28 @@
+from huggingface_hub import snapshot_download
+from pathlib import Path
+import shutil
+
+snapshot_download(
+    repo_id="alibaba-pai/Wan2.1-Fun-1.3B-Control",
+    local_dir="checkpoints/Wan2.1-Fun-1.3B-Control",
+    local_dir_use_symlinks=False,
+    resume_download=True,
+)
+
+print("Downloaded to checkpoints/Wan2.1-Fun-1.3B-Control")
+
+dst = Path("checkpoints/illumicraft_pretrained_weights")
+snapshot_download(
+    repo_id="YuanzeLin/Illumicraft-checkpoints",
+    local_dir=dst,
+    allow_patterns="checkpoint/*",
+)
+
+checkpoint_dir = dst / "checkpoint"
+
+for p in checkpoint_dir.iterdir():
+    shutil.move(str(p), str(dst / p.name))
+
+checkpoint_dir.rmdir()
+
+print("Downloaded to checkpoints/illumicraft_weights")
