02eed7cdea481e401741e4e47bd633e22f556955
Yuanze Lin <yuanze.lin@cs.ox.ac.uk>
2026-06-06 00:14:57 +0100

    Update run_gradio_demo.sh

--- run_gradio_demo.sh
+++ run_gradio_demo.sh
@@ -4,7 +4,7 @@ export CUDA_VISIBLE_DEVICES=0
 WAN_MODEL_PATH="checkpoints/Wan2.1-Fun-1.3B-Control"
 ILLUMICRAFT_CKPT_PATH="checkpoints/illumicraft_pretrained_weights"
 
-python app.py \
+python utils/app.py \
   --wan_model_path "$WAN_MODEL_PATH" \
   --illumicraft_ckpt_path "$ILLUMICRAFT_CKPT_PATH" \
   --config_path config/wan.yaml \
