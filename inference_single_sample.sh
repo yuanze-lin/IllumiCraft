9e3e283cec6b6d24f8915ecfe0a4db3054618ebf
Yuanze Lin <yuanze.lin@cs.ox.ac.uk>
2026-06-05 22:27:59 +0100

    Update inference_single_sample.sh

--- inference_single_sample.sh
+++ inference_single_sample.sh
@@ -7,10 +7,10 @@ OUTPUT_PATH="demo/single_sample_outputs"
 
 FOREGROUND_VIDEO_PATH="demo/eval/foreground_videos_00000.mp4"
 FOREGROUND_PROMPT="A majestic waterfall cascades down a rugged cliff into a serene pool."
+LIGHTING_PROMPT="Cool-blue spotlights beam through mist onto a central pool of light, creating high-contrast cinematic depth and a moody, immersive atmosphere."
 
-# Optional: enable background-conditioned generation
-BACKGROUND_PATH=""  # optional
-LIGHTING_PROMPT="Cool-blue spotlights beam through mist onto a central pool of light, creating high-contrast cinematic depth and a moody, immersive atmosphere."   # optional
+# Optional background-conditioned generation
+BACKGROUND_PATH=""
 
 python testing/inference_single_sample.py \
     --pretrained_model_name_or_path "$WAN_MODEL_PATH" \
@@ -18,6 +18,6 @@ python testing/inference_single_sample.py \
     --model_path "$ILLUMICRAFT_CKPT_PATH" \
     --foreground_video_path "$FOREGROUND_VIDEO_PATH" \
     --base_prompt "$FOREGROUND_PROMPT" \
+    --lighting_prompt "$LIGHTING_PROMPT" \
     --output_path "$OUTPUT_PATH" \
-    ${LIGHTING_PROMPT:+--lighting_prompt "$LIGHTING_PROMPT"} \
     ${BACKGROUND_PATH:+--background_path "$BACKGROUND_PATH"}
